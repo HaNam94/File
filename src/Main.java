1d6bdb439be52c9575388b340918e9fb4cfef31e
HaNam94 <haduynam.94@gmail.com>
2024-04-26 16:10:58 +0700

    IO:TextFile

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,49 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//        ReadAndWriteFile readAndWriteFile = new ReadAndWriteFile();
-//        List<Integer> numbers = readAndWriteFile.readFile("numbers.txt");
-//        int maxValue = findMax(numbers);
-//        readAndWriteFile.writeFile("result.txt", maxValue);
+        BufferedReader bufferedReader = null;
+        try {
+            String line;
+            bufferedReader  = new BufferedReader(new FileReader("countries.csv"));
+            while ((line = bufferedReader.readLine()) != null){
+                printCountry(parseCsvLine(line));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bufferedReader != null)
+                    bufferedReader.close();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+    public static List<String> parseCsvLine(String csvLine){
+        List<String> result = new ArrayList<>();
+        if (csvLine != null){
+            String[] splitData = csvLine.split(",");
+            for (int i = 0; i < splitData.length; i++) {
+                result.add(splitData[i]);
+            }
+        }
+        return result;
+    }
+    private static void printCountry(List<String> country){
+        System.out.println(
+                "Country [id= "
+                        + country.get(0)
+                        + ", code= " + country.get(1)
+                        + ", name=" + country.get(2)
+                        + "]"
+
+        );
     }
 }
